c36d8c0cf2d3d32852fba9a30606d90b726964bb
Ephirious <Ephirious@gmail.com>
2025-04-04 11:39:26 +0300

    feat(Coordinates): add Coordinates for Entities
    The class Coordinates has 2 fields: X and Y position
    This class helps to display Entities on a Map with Cartesian coordinates

---
+++ src/util/Coordinates.java
@@ -0,0 +1,41 @@
+package util;
+
+import java.util.Objects;
+
+public class Coordinates {
+    private int posX;
+    private int posY;
+
+    public Coordinates(int posX, int posY) {
+        this.posX = posX;
+        this.posY = posY;
+    }
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public void setPosX(int posX) {
+        this.posX = posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
+    public void setPosY(int posY) {
+        this.posY = posY;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Coordinates that = (Coordinates) o;
+        return posX == that.posX && posY == that.posY;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(posX, posY);
+    }
+}
